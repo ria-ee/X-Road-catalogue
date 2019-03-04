0c3c78dad9f8e304ed761a890b45efc1c1b3ea7a
Vitali Stupin <vitali.stupin@ria.ee>
2019-03-04 14:35:54 +0200

    new favicon, fix for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,10 @@ import { SubsystemComponent } from './subsystem/subsystem.component';
 
 const routes: Routes = [
   { path: '', component: SubsystemListComponent },
-  // Redirecting old catalogue app links
-  { path: ':instance/wsdls', redirectTo: '/:instance', pathMatch: 'full' },
   { path: ':instance', component: SubsystemListComponent },
   { path: ':instance/:class/:member/:subsystem', component: SubsystemComponent },
-  { path: '**', redirectTo: '/' }
+  // Redirecting old catalogue app links
+  { path: ':instance', redirectTo: '/:instance', pathMatch: 'prefix' }
 ];
 
 @NgModule({
