4297ea4d648b6f2a5f96510313a880c12a0e0aae
Vitali Stupin <vitali.stupin@ria.ee>
2019-03-25 14:06:15 +0200

    Fixing coverage directory name

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/methods'),
+      dir: require('path').join(__dirname, '../coverage/xtss-catalogue'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
