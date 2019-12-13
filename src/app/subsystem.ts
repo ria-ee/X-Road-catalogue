f8fb49298ac2028030f692abf3693bd5bb34c3cf
Vitali Stupin <vitali.stupin@ria.ee>
2019-12-13 13:19:15 +0200

    Adding support for REST services

--- src/app/subsystem.ts
+++ src/app/subsystem.ts
@@ -1,11 +1,14 @@
 import { Method } from './method';
+import { Service } from './service';
 
 export class Subsystem {
     memberClass: string;
     subsystemCode: string;
     xRoadInstance: string;
     subsystemStatus: string;
+    servicesStatus: string;
     memberCode: string;
     fullSubsystemName: string;
     methods: Method[];
+    services: Service[];
 }
